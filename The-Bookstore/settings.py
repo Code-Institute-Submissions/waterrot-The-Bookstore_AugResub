b4df5f42c78c29b81ee1d586fd190572a866bf24
waterrot <oudloosdrecht@gmail.com>
2021-08-13 15:21:49 +0000

    added bag context and delivery logic

--- The-Bookstore/settings.py
+++ The-Bookstore/settings.py
@@ -70,7 +70,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
+                'bag.contexts.bag_contents',
+            ], 
         },
     },
 ]
@@ -156,3 +157,6 @@ if 'USE_AWS' in os.environ:
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
