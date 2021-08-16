e6ac968069a0f9e3a8f99c2b80cc0e3bfce9d581
waterrot <oudloosdrecht@gmail.com>
2021-08-16 19:41:09 +0000

    added 2 new fields to the book products

--- products/models.py
+++ products/models.py
@@ -25,6 +25,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2,
                                  null=True, blank=True)
+    author = models.CharField(max_length=254, null=True, blank=True)
+    new_release = models.BooleanField(default=False, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
