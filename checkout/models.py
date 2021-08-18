f0132b1817b7e6cb196a0ed0c4013a8a6a1f7b45
waterrot <oudloosdrecht@gmail.com>
2021-08-18 20:36:07 +0000

    added orderline model basic variables

--- checkout/models.py
+++ checkout/models.py
@@ -22,3 +22,11 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_format = models.CharField(max_length=2, null=True, blank=True) # ebook, audiobook, softcover, hardcover
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
