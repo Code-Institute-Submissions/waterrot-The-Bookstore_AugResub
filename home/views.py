8e8ff89b2d00369a3a3ccbb43923361e757f730a
waterrot <oudloosdrecht@gmail.com>
2021-08-23 08:21:33 +0000

    added first book to homepage

--- home/views.py
+++ home/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 
 
 # Create your views here.
 def index(request):
     """ A view to return the index page """
 
-    return render(request, "home/index.html")
+    new_release_1 = get_object_or_404(Product, pk=25)
+    new_release_2 = get_object_or_404(Product, pk=83)
+    new_release_3 = get_object_or_404(Product, pk=99)
+
+    context = {
+        'new_release_1': new_release_1,
+        'new_release_2': new_release_2,
+        'new_release_3': new_release_3,
+    }
+
+    return render(request, "home/index.html", context)
